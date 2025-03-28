dd570e47dbe67eb6ee8fe7a6a48b44c5010b4d1f
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-03-28 21:13:39 +0700

    Update homebrew.nix

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,4 +1,9 @@
 _: {
+  nix-homebrew = {
+    enable = true;
+    user = "phuc";
+    autoMigrate = true;
+  };
   homebrew = {
     enable = true;
     casks = [
@@ -6,17 +11,5 @@ _: {
       "ghostty"
     ];
     onActivation.cleanup = "zap"; # Declarative nature of homebrew
-  };
-  nix-homebrew = {
-    # Install Homebrew under the default prefix
-    enable = true;
-
-    # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-
-    # User owning the Homebrew prefix
-    user = "phuc";
-
-    # Automatically migrate existing Homebrew installations
-    autoMigrate = true;
   };
 }
