6a9e3e26b3350ca9e6c2e32f6fe876733df7d698
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-03-28 11:35:20 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -12,11 +12,6 @@
     nixos-unified.url = "github:srid/nixos-unified";
 
     # Software inputs
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.inputs.flake-parts.follows = "flake-parts";
   };
 
   # Wired using https://nixos-unified.org/autowiring.html
