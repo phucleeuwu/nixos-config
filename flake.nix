47ca187d02a7cb709a6cc59b4363a5da03784a86
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-03-28 11:37:29 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,22 @@
   description = "A home-manager template providing useful tools & settings for Nix-based development";
 
   inputs = {
-    # Principle inputs (updated by `nix run .#update`)
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-unified.url = "github:srid/nixos-unified";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Software inputs
+    catppuccin.url = "github:catppuccin/nix";
   };
 
-  # Wired using https://nixos-unified.org/autowiring.html
   outputs = inputs:
     inputs.nixos-unified.lib.mkFlake
     {
