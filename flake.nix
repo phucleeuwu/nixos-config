3e6d9a401ad9ec5390c040e6422326e2f40b622a
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-03-28 20:38:27 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A home-manager template providing useful tools & settings for Nix-based development";
+  description = "Auto Wire nixos config";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
