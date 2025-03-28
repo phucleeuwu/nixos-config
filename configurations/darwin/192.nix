965f3e0b6616d288ce43207b7336b0acdd0e69ca
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-03-28 11:17:22 +0700

    Update 192.nix

--- configurations/darwin/192.nix
+++ configurations/darwin/192.nix
@@ -6,15 +6,76 @@ in {
   imports = [
     self.darwinModules.default
   ];
-
   nixpkgs.hostPlatform = "aarch64-darwin";
-
-  system.keyboard = {
-    enableKeyMapping = true;
-    remapCapsLockToControl = true;
-  };
-
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+
+  #Macos settings
+  system.defaults = {
+    LaunchServices.LSQuarantine = false;
+    controlcenter.BatteryShowPercentage = true;
+    ".GlobalPreferences"."com.apple.mouse.scaling" = 3.0;
+
+    trackpad = {
+      Clicking = true;
+      ActuationStrength = 0;
+    };
+
+    loginwindow = {
+      GuestEnabled = false;
+      ShutDownDisabled = true;
+      SleepDisabled = true;
+      RestartDisabled = true;
+      ShutDownDisabledWhileLoggedIn = true;
+      PowerOffDisabledWhileLoggedIn = true;
+      RestartDisabledWhileLoggedIn = true;
+      DisableConsoleAccess = true;
+    };
+
+    dock = {
+      tilesize = 40;
+      autohide-delay = 0.0;
+      autohide-time-modifier = 0.0;
+      largesize = 80;
+      mru-spaces = false;
+      show-recents = false;
+      magnification = true;
+      showhidden = true;
+      autohide = true;
+      static-only = true;
+    };
+
+    finder = {
+      FXPreferredViewStyle = "clmv";
+      FXDefaultSearchScope = "SCcf";
+      NewWindowTarget = "Home";
+      CreateDesktop = false;
+      ShowStatusBar = true;
+      ShowPathbar = true;
+      FXRemoveOldTrashItems = true;
+    };
+
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      InitialKeyRepeat = 15;
+      AppleKeyboardUIMode = 3;
+      KeyRepeat = 2;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      _HIHideMenuBar = true;
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
+      "com.apple.sound.beep.volume" = 0.6065307;
+      "com.apple.trackpad.scaling" = 3.0;
+      "com.apple.swipescrolldirection" = false;
+    };
+  };
+
+  services = {
+    # karabiner-elements.enable = true;
+  };
 }
