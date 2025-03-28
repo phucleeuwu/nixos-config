3600259a3c75b1085358187c9be7511809d0fabe
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-03-28 21:18:33 +0700

    Update common.nix

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -19,8 +19,6 @@ in {
     settings = {
       max-jobs = "auto";
       experimental-features = "nix-command flakes";
-      # I don't have an Intel mac.
-      extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
       # Nullify the registry for purity.
       flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
       trusted-users = [
