3d5f60108397f79c9f08794018d0da11c3bed844
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-03-28 11:29:52 +0700

    Update 192.nix

--- configurations/darwin/192.nix
+++ configurations/darwin/192.nix
@@ -5,77 +5,22 @@
 in {
   imports = [
     self.darwinModules.default
+    ./system.nix
   ];
   nixpkgs.hostPlatform = "aarch64-darwin";
-  # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+  security.pam.services.sudo_local.touchIdAuth = true;  
 
-  #Macos settings
-  system.defaults = {
-    LaunchServices.LSQuarantine = false;
-    controlcenter.BatteryShowPercentage = true;
-    ".GlobalPreferences"."com.apple.mouse.scaling" = 3.0;
+  programs.fish.enable = true; # macOs default shell
+  environment.shells = [pkgs.fish];
 
-    trackpad = {
-      Clicking = true;
-      ActuationStrength = 0;
-    };
-
-    loginwindow = {
-      GuestEnabled = false;
-      ShutDownDisabled = true;
-      SleepDisabled = true;
-      RestartDisabled = true;
-      ShutDownDisabledWhileLoggedIn = true;
-      PowerOffDisabledWhileLoggedIn = true;
-      RestartDisabledWhileLoggedIn = true;
-      DisableConsoleAccess = true;
-    };
-
-    dock = {
-      tilesize = 40;
-      autohide-delay = 0.0;
-      autohide-time-modifier = 0.0;
-      largesize = 80;
-      mru-spaces = false;
-      show-recents = false;
-      magnification = true;
-      showhidden = true;
-      autohide = true;
-      static-only = true;
-    };
-
-    finder = {
-      FXPreferredViewStyle = "clmv";
-      FXDefaultSearchScope = "SCcf";
-      NewWindowTarget = "Home";
-      CreateDesktop = false;
-      ShowStatusBar = true;
-      ShowPathbar = true;
-      FXRemoveOldTrashItems = true;
-    };
-
-    NSGlobalDomain = {
-      AppleInterfaceStyle = "Dark";
-      InitialKeyRepeat = 15;
-      AppleKeyboardUIMode = 3;
-      KeyRepeat = 2;
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = false;
-      _HIHideMenuBar = true;
-      AppleShowAllFiles = true;
-      AppleShowAllExtensions = true;
-      "com.apple.sound.beep.volume" = 0.6065307;
-      "com.apple.trackpad.scaling" = 3.0;
-      "com.apple.swipescrolldirection" = false;
-    };
-  };
-
-  services = {
-    # karabiner-elements.enable = true;
+  homebrew = {
+    enable = true;
+    casks = [
+      "karabiner-elements"
+      "ghostty"
+    ];
+    onActivation.cleanup = "zap"; # Declarative nature of homebrew
   };
 }
