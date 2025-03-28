13ff8c8b80547570a342acb388af74e2ee167752
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-03-28 17:45:15 +0700

    Update common.nix

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -1,17 +1,33 @@
 # Common to Linux & darwin
-{flake, ...}: let
+{ flake, pkgs, lib, ... }:
+
+let
   inherit (flake) inputs;
   inherit (inputs) self;
-in {
+in
+{
   home-manager = {
     backupFileExtension = "backup";
     useUserPackages = true;
     useGlobalPkgs = true;
   };
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
   nixpkgs.config.allowUnfree = true;
-  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  nix = {
+    # Choose from https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=nix
+    # package = pkgs.nixVersions.latest;
+
+    nixPath = [ "nixpkgs=${flake.inputs.nixpkgs}" ]; # Enables use of `nix-shell -p ...` etc
+    registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
+
+    settings = {
+      max-jobs = "auto";
+      experimental-features = "nix-command flakes";
+      # I don't have an Intel mac.
+      extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
+      # Nullify the registry for purity.
+      flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
+      trusted-users = [ "root" (if pkgs.stdenv.isDarwin then flake.config.me.username else "@wheel") ];
+    };
+  };
 }
