5598ba6137546710c6642b20faa9ef4cf36e1acf
phucleeuwu <phucleeuwu@gmail.com>
2025-03-29 21:48:40 +0700

    remove some lazyvim plugins

--- modules/home/cli/neovim.nix
+++ modules/home/cli/neovim.nix
@@ -2,28 +2,8 @@ _: {
   programs.lazyvim = {
     enable = true;
     extras = {
-      coding = {
-        yanky.enable = true;
-      };
-
-      editor = {
-        dial.enable = true;
-
-        inc-rename.enable = true;
-      };
-
       lang = {
         nix.enable = true;
-      };
-
-      test = {
-        core.enable = true;
-      };
-
-      util = {
-        dot.enable = true;
-
-        mini-hipatterns.enable = true;
       };
     };
   };
