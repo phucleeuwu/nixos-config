69f86b6257400350a5557c70fe692e28f84fd66e
phuc <phucleeuwu@gmail.com>
2025-03-30 16:19:48 +0700

    theme from catppuccin

--- modules/home/gui/wezterm.nix
+++ modules/home/gui/wezterm.nix
@@ -9,9 +9,8 @@ _: {
       local config = {}
       config.font = wezterm.font 'Maple Mono NF'
       config.font_size = 19
-      config.enable_tab_bar = false
       config.window_decorations = "RESIZE"
-      config.color_scheme = "Catppuccin Mocha"
+      config.enable_tab_bar = false
       config.window_close_confirmation = 'NeverPrompt'
       return config
     '';
