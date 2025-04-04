882b550a191aa8a89d1850b63069622fb2f30247
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com.>
2025-04-04 09:52:47 +0700

    what am i doing

--- config.nix
+++ config.nix
@@ -2,7 +2,7 @@
 {
   me = {
     username = "phuc";
-    fullname = "phuclee";
+    fullname = "phucleeuwu";
     email = "125681538+phucleeuwu@users.noreply.github.com.";
     sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
   };
