c89e67f2eb3a453f8789ec17b16eedc0dac56197
phuclee <srid@srid.ca>
2025-04-04 09:46:14 +0700

    update fake email

--- config.nix
+++ config.nix
@@ -3,7 +3,7 @@
   me = {
     username = "phuc";
     fullname = "phuclee";
-    email = "srid@srid.ca";
+    email = "phuc@lee";
     sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
   };
 }
