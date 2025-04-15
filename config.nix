32d100b7b74125229984d315096044fcfd74b8b2
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com.>
2025-04-15 22:55:44 +0700

    nice

--- config.nix
+++
@@ -1,9 +0,0 @@
-# See ./modules/flake/config.nix for module options.
-{
-  me = {
-    username = "example";
-    fullname = "phucleeuwu";
-    email = "125681538+phucleeuwu@users.noreply.github.com.";
-    sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
-  };
-}
