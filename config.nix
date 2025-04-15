b5ea1790c425f7eef6623fc29260fbd27d3ac361
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com.>
2025-04-15 22:31:42 +0700

    dotflow auto change username for config.nix

--- config.nix
+++ config.nix
@@ -1,7 +1,7 @@
 # See ./modules/flake/config.nix for module options.
 {
   me = {
-    username = "phuc";
+    username = "example";
     fullname = "phucleeuwu";
     email = "125681538+phucleeuwu@users.noreply.github.com.";
     sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
